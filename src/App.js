171fe71221b4d78610fd9c7afd2f6c3eb7f5550a
Robério Almeida <dev.roberio@gmail.com>
2022-02-26 02:02:48 -0300

    feat: cria função onSaveButtonClick que limpa o Form

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ class App extends Component {
     };
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
     this.validate = this.validate.bind(this);
   }
 
@@ -27,6 +28,19 @@ class App extends Component {
     this.setState({
       [name]: inputValue,
     }, this.validate);
+  }
+
+  onSaveButtonClick() {
+    this.setState(() => ({
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: 0,
+      cardAttr2: 0,
+      cardAttr3: 0,
+      cardImage: '',
+      cardRare: '',
+      cardTrunfo: false,
+    }));
   }
 
   validate() {
